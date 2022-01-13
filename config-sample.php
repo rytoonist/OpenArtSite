26d9ffb9d8d00090884c5ff594a9f6c3f03ac9a1
ry <2892016+rytoonist@users.noreply.github.com>
2022-01-13 20:03:55 +0000

    Create config-sample.php

---
+++ config-sample.php
@@ -0,0 +1,14 @@
+<?php
+
+  return [
+    "database" => [
+      "primary" => [
+        "driver" => "mysql",
+        "host" => "localhost",
+        "port" => 3306,
+        "dbname" => "my_art_site",
+        "username" => "root",
+        "password" => "",
+      ],
+    ],
+  ];
